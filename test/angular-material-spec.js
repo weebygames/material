0951a5a83ff1fe5020bf3478e07a0ca54df7f928
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-08-04 10:52:58 -0500

    update(tests): load ngAnimate before material animate mocks

--- test/angular-material-spec.js
+++ test/angular-material-spec.js
@@ -28,13 +28,16 @@
     };
   }
 
+
+
   beforeEach(function() {
 
     /**
      * Before each test, require that the 'ngMaterial-mock' module is ready for injection
      * NOTE: assumes that angular-material-mocks.js has been loaded.
      */
-    module('ngMaterial-mock')
+    module('ngAnimate');
+    module('ngMaterial-mock');
 
     /**
      * Mocks angular.element#focus ONLY for the duration of a particular test.
